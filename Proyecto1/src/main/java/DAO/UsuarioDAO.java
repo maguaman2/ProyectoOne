693aa7416e342f6611e8cfb590128b2800b5f3ce
maguaman <maguaman@engicoders.com>
2020-07-07 09:35:51 -0500

    Se agrega la clase usuario DAO

---
+++ Proyecto1/src/main/java/DAO/UsuarioDAO.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+/**
+ *
+ * @author magb
+ */
+public class UsuarioDAO {
+    private int codigo;
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(int codigo) {
+        this.codigo = codigo;
+    }
+    
+    
+}
